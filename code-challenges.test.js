1f2d7f0409ef889e0b01a3c591f5d426970db0a6
lundrew <andrueee@gmail.com>
2022-04-24 21:28:32 -0700

    Complete Week 5

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,8 @@
 // ASSESSMENT 5: JavaScript Coding Practical Questions with Jest
 
+// const { it, expect } = require("@jest/globals")
+// const { describe } = require("yargs")
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -11,9 +14,15 @@
 // Run the file with the following command: $ yarn jest
 
 
-// --------------------1) Create a function that takes in a string and returns a coded message. The coded message converts 'a' to 4, 'e' to 3, 'i' to 1, and 'o' to 0.
 
-// a) Create a test with expect statements using the variables provided.
+
+
+
+
+
+// // --------------------1) Create a function that takes in a string and returns a coded message. The coded message converts 'a' to 4, 'e' to 3, 'i' to 1, and 'o' to 0.
+
+// // a) Create a test with expect statements using the variables provided.
 
 const secretCodeWord1 = "Lackadaisical"
 // Expected output: "L4ck4d41s1c4l"
@@ -23,12 +32,52 @@ const secretCodeWord3 = "Eccentric"
 // Expected output: "3cc3ntr1c"
 
 
-// b) Create the function that makes the test pass.
+describe('coded', () => {
+    const secretCodeWord1 = "Lackadaisical"
+    const secretCodeWord2 = "Gobbledygook"
+    const secretCodeWord3 = "Eccentric"
+    it('takes in a string and returns a coded message.', () => {
+        expect(coded(secretCodeWord1)).toEqual("L4ck4d41s1c4l");
+        expect(coded(secretCodeWord2)).toEqual("G0bbl3dyg00k");
+        expect(coded(secretCodeWord3)).toEqual("3cc3ntr1c");
+    });
+});
 
 
-// --------------------2) Create a function that takes in an array of words and a single letter and returns all the words that contain that particular letter.
+// // b) Create the function that makes the test pass.
 
-// a) Create a test with expects statement using the variable provided.
+// Pseudo:
+// create function named "coded"
+// input param named "string"
+// create new variable called "newstring" and make it equal to param "string"
+// use the replace() method on param "string" to exchange letters
+// create another variable named "newstring2" and make it equal to "newstring" and use the replace() method to exchange the next letter. create new variable for each letter that needs to be exchanged. 
+
+const coded = (string) => {
+
+    let newstring = string.replace(/a|A/g, "4")
+    let newstring2 = newstring.replace(/e|E/gi, "3")
+    let newstring3 = newstring2.replace(/i|I/gi, "1")
+    let newstring4 = newstring3.replace(/o|O/gi, "0")
+
+    return newstring4
+    
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+// // --------------------2) Create a function that takes in an array of words and a single letter and returns all the words that contain that particular letter.
+
+// // a) Create a test with expects statement using the variable provided.
 
 const arrayOfWords1 = ["Apple", "Banana", "Plum", "Orange", "Kiwi"]
 const letterA = "a"
@@ -38,7 +87,44 @@ const letterE = "e"
 // Expected output: ["Cherry", "Blueberry", "Peach"]
 
 
-// b) Create the function that makes the test pass.
+
+describe('specific', () => {
+    const arrayOfWords1 = ["Apple", "Banana", "Plum", "Orange", "Kiwi"]
+    const letterA = "a"
+    const arrayOfWords2 = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach"]
+    const letterE = "e"
+    it('takes in an array of words and a single letter and returns all the words that contain that particular letter.', () => {
+        expect(specific(arrayOfWords1, letterA)).toEqual(["Apple", "Banana", "Orange"]);
+        expect(specific(arrayOfWords2, letterE)).toEqual(["Cherry", "Blueberry", "Peach"]);
+    });
+});
+
+
+// // b) Create the function that makes the test pass.
+
+// Pseudo:
+// create a function called "specific"
+// create 2 params named "array" and "letter"
+// return the array that uses the filter() method 
+// verify the string includes a specific letter using the includes() method on the "letter" parameter and use an "or" to check uppercase letters.
+
+const specific = (array, letter) => {
+
+    return array.filter(string => { return string.includes(letter) || string.includes(letter.toUpperCase())}) 
+
+   }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a “full house”. A full house is exactly one pair and one three of a kind.
@@ -52,5 +138,57 @@ const hand2 = [5, 5, 3, 3, 4]
 const hand3 = [5, 5, 5, 5, 4]
 // Expected output: false
 
+describe('fullhouse', () => {
+    const hand1 = [5, 5, 5, 3, 3]
+    const hand2 = [5, 5, 3, 3, 4]
+    const hand3 = [5, 5, 5, 5, 4]
+    it('takes in an array of 5 numbers and determines whether or not the array is a “full house”.', () => {
+        expect(fullhouse(hand1)).toBeTruthy();
+        expect(fullhouse(hand2)).toBeFalsy();
+        expect(fullhouse(hand3)).toBeFalsy();
+    })
+})
 
 // b) Create the function that makes the test pass.
+
+// Pseudo:
+// create a function named "fullhouse" that takes in a parameter named "array"
+// create an if else statement to determine whether index "0" is equal to index "1" and "2" for the three of a kind
+// add an "&&" to determine if index "3" is equal to index "4" for the pair and return true if condition is met 
+// add an "or" to determine if index "0" is equal to index "2" and "3" for the three of a kind, add an "&&" to determine if index "1" is equal to index "4" for the pair and return true if condition is met 
+// add an "or" to determine if index "0" is equal to index "3" and "4" for the three of a kind, add an "&&" to determine if index "1" is equal to index "2" for the pair and return true if condition is met 
+// else return false
+
+const fullhouse = (array) => {
+
+    if (array[0] === array[1,2] && array[3] === array[4] || array[0] === array[2,3] && array[1] === array[4] || array[0] === array[3,4] && array[1] === array[2])
+        
+        return true
+
+    else{
+
+        return false
+
+    }
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- interview-questions.md
+++ interview-questions.md
@@ -7,53 +7,53 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is a hash in Ruby?
 
-  Your answer:
+  Your answer: Hash in Ruby is a a collection of keys and values. 
 
-  Researched answer:
+  Researched answer: A Hash in Ruby is a collection of key-value pairs. A Hash allows us to store multiple pieces of information. 
 
 
 
 2. What is a gem?
 
-  Your answer:
+  Your answer: Gem's in Ruby are like libraries similar to how Reactjs is to Javascript. 
 
-  Researched answer:
+  Researched answer: A Gem is like a plugin or an extension for the language Ruby. With RubyGems you can install multiple gem's which are packaged libraries. A location to find where libraries are hosted is RubyGems.org. This is a public repository of gems that can be searched and installed on a machine. You can either search for gems using the Rubygems website or the "gem" command. 
 
 
 
 3. What is Ruby on Rails?
 
-  Your answer:
+  Your answer: Ruby on Rails is an open source framework. Ruby on Rails helps build web applications in a more seamless way. 
 
-  Researched answer:
+  Researched answer: Ruby on Rails is a framework which contains a collection of code, tools and utilities that give a specific structure to work with when writing code. Rails helps build websites and contains something called Active Record which helps you read, create and update records in a database. Some exmaples of Ruby on Rails Applications are github, shopify, ask.fm, kickstarter, twitch, instacart and soundcloud. 
 
 
 
 4. What is a relational database? Are there other kinds of databases?
 
-  Your answer:
+  Your answer: A relational database is a collection of information that organizes the information where the database then assigns a unique ID.
 
-  Researched answer:
+  Researched answer: A relational database stores and provides access to data points that may be related to one another. The data is represented in tables where each row in the table is a record with a unique ID called a key. Each column of the tables holds attributes of the data and each record has a value for each attribute. This makes it easier to establish the relationships among the data points. 
 
 
 
 5. What are primary keys? Why are they important?
 
-  Your answer:
+  Your answer: When a new instance of a class is created, a primary key is given to that instance which is an ID. 
 
-  Researched answer:
+  Researched answer: A primary key is a a single field or combination of fields that uniquely defines a record. Databases use keys to sort, compare and store records and create relationships between records. 
 
 
 
 ## Looking Ahead: Terms for Next Week
 Research and define the following terms to the best of your ability.
 
-1. RESTful routes:
+1. RESTful routes: REST stands for Representational State Transfer. REST is a pattern to follow when we structure routes between the server and the client. Whenever we navigate through a site by clicking the links,  
 
-2. JSON:
+2. JSON: JSON or Javascript Object Notation is commonly used to transmit data in web applications. This means sending some data from the server to the client, so it can then be displated on a web page. 
 
-3. ERB:
+3. ERB: ERB refers to Embedded Ruby. This serves as a template engine in which Ruby language embeds into HTML. With ERB, we are able to use Ruby code within HTML.
 
-4. Params:
+4. Params: Params, or URL parameters are added to the end of a URL after a '?' symbol where multiple parameters can be included when separated by the '&' symbol. Params are used for sorting and filtering, pagination, site search, translation, describing and tracking. 
 
-5. API:
+5. API: API stands for Application Programming Interface. API is a software intermediary which allows two applications to work with eachother.
